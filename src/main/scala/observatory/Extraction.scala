8987f1b5eadefa8428becc379bcce860788f7552
Javier <jahnog@gmail.com>
2017-08-18 00:40:03 -0300

    upd.

--- src/main/scala/observatory/Extraction.scala
+++ src/main/scala/observatory/Extraction.scala
@@ -18,17 +18,64 @@ object Extraction {
     */
   def locateTemperatures(year: Int, stationsFile: String, temperaturesFile: String): Iterable[(LocalDate, Location, Double)] = {
 
-    val sf: InputStream = getClass().getResourceAsStream(stationsFile)
+    val stationsReader = new BufferedSource(getClass().getResourceAsStream(stationsFile))
 
-    val read = new BufferedSource(sf)
+    val stations = Map[(String, String), (Double, Double)]()
 
-    val x = read
+    val allStations = stationsReader
       .getLines()
       .map(str => {
-        (LocalDate.now(), new Location(0d, 0d), 0d)
+        val lineArr = str.split(",")
+        var stn = ""
+        var wban = ""
+        var lat = -100d
+        var long = -100d
+        if (lineArr.size > 0) {
+          stn = lineArr(0)
+        }
+        if (lineArr.size > 1) {
+          wban = lineArr(1)
+        }
+        if (lineArr.size > 2) {
+          lat = lineArr(2).toDouble
+        }
+        if (lineArr.size > 3) {
+          long = lineArr(3).toDouble
+        }
+        (stn, wban, lat, long)
+      })
+      .filter(row => row._3 > -100d && row._4 > -100d)
+      .foldLeft(stations)((stations: Map[(String, String), (Double, Double)], station: (String, String, Double, Double)) => {
+        val newStations = stations.updated((station._1, station._2), (station._3, station._4))
+        newStations
       })
 
-    x.toIterable
+
+    println(s"Stations count: ${allStations.size}")
+
+    val tempReader = new BufferedSource(getClass().getResourceAsStream(temperaturesFile))
+
+    val allTemps = tempReader
+      .getLines()
+      .map(str => {
+        val lineArr = str.split(",")
+        (lineArr(0), lineArr(1), lineArr(2).toInt, lineArr(3).toInt, lineArr(4).toDouble)
+      })
+      .map(tmp => {
+
+        try {
+          val station = allStations((tmp._1, tmp._2))
+          val date = LocalDate.of(year, tmp._3, tmp._4)
+          val location = Location(station._1, station._2)
+          (date, location, tmp._5)
+        } catch {
+          case ex: java.util.NoSuchElementException => (LocalDate.now(), Location(-100d, -100d), -100d)
+
+        }
+      })
+      .filter(r => r._2.lat > -100d && r._2.lon > -100d)
+
+    allTemps.toIterable
   }
 
   /**
