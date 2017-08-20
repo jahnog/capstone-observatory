8a5988fe1ada18042ac24f13a6d964afc2fac5c6
Javier <jahnog@gmail.com>
2017-08-20 16:09:23 -0300

    Added test.

--- src/test/scala/observatory/VisualizationTest.scala
+++ src/test/scala/observatory/VisualizationTest.scala
@@ -6,5 +6,12 @@ import org.scalatest.prop.Checkers
 
 trait VisualizationTest extends FunSuite with Checkers {
 
+  // Color(255,0,0). Expected: Color(191,0,64) (scale = List((-1.0,Color(255,0,0)), (0.0,Color(0,0,255))), value = -0.75)
 
+  test("Linear Color interpolation") {
+    val scale = List((-1.0,Color(255,0,0)), (0.0,Color(0,0,255)))
+    val value = -0.75
+
+    assert(Color(191,0,64) == Visualization.interpolateColor(scale, value))
+  }
 }
